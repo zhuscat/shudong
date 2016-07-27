d8acf56e3213b22c58e774b175b5b01ff1e50043
unknown <>
2016-07-27 23:51:38 +0800

    update back end management

--- static/js/back-stage-management.js
+++ static/js/back-stage-management.js
@@ -9,7 +9,7 @@ function delete_comment(comment_id){
                 },
                 success:function(data){
                     if(data.success){
-                    alert("test order");
+                    alert("delete success");
                     //all delete is success,this can be execute
                     //$("#tr_"+comment_id).remove();
                     $(this).css("background","#dedada");
@@ -17,4 +17,93 @@ function delete_comment(comment_id){
                 }
             });
         }
-    };
+};
+
+function gag(user_id){
+ confirm_=confirm('This action will gag! Are you sure?');
+        if(confirm_){
+            $.ajax({
+                type:"POST",
+                url:'../static/data/banCommitComment.json',
+                data:{
+                    id:user_id
+                },
+                success:function(data){
+                    if(data.success){
+                    alert("gag success");
+                    //all delete is success,this can be execute
+                    //$("#tr_"+comment_id).remove();
+                    $(this).css("background","#5cb85c");
+                    $(this).innerHTML="解除禁言";
+                }
+                }
+            });
+        }
+};
+
+function removegag(user_id){
+ confirm_=confirm('This action will remove the gag! Are you sure?');
+        if(confirm_){
+            $.ajax({
+                type:"POST",
+                url:'../static/data/removegag.json',
+                data:{
+                    id:user_id
+                },
+                success:function(data){
+                    if(data.success){
+                    alert("remove the gag success");
+                    //all delete is success,this can be execute
+                    //$("#tr_"+comment_id).remove();
+                    $(this).css("background","#d9534f");
+                    $(this).innerHTML="禁言";
+                }
+                }
+            });
+        }
+};
+
+function undercarriage(book_id){
+ confirm_=confirm('This action will remove the book! Are you sure?');
+        if(confirm_){
+            $.ajax({
+                type:"POST",
+                url:'../static/data/undercarriage.json',
+                data:{
+                    id:user_id
+                },
+                success:function(data){
+                    if(data.success){
+                    alert("remove the book success");
+                    //all delete is success,this can be execute
+                    //$("#tr_"+comment_id).remove();
+                    $(this).css("background","#d9534f");
+                    $(this).innerHTML="已下架";
+                }
+                }
+            });
+        }
+};
+
+
+function deletebook(book_id){
+ confirm_=confirm('This action will delete the book! Are you sure?');
+        if(confirm_){
+            $.ajax({
+                type:"POST",
+                url:'../static/data/deletebook.json',
+                data:{
+                    id:book_id
+                },
+                success:function(data){
+                    if(data.success){
+                    alert("delete the book success");
+                    //all delete is success,this can be execute
+                    //$("#tr_"+comment_id).remove();
+                    $(this).css("background","#fafafa");
+                    $(this).innerHTML="已删除";
+                }
+                }
+            });
+        }
+};
